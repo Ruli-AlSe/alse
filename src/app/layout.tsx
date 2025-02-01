fbfc8e57d32a3f0e6994bca949d5cd942f2abfc4
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-02-01 03:35:13 -0600

    Remove MDX editor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { Toaster } from '@/components/ui/toaster';
 
 import { ThemeProvider } from '@/components/theme/theme-provider';
 
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
