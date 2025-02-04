2ec28cfce4cea81b4672c04bb278177c3b093be4
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-02-04 04:10:53 -0600

    Install Tiptap extensions for creating links and higlight texts

--- src/interfaces/rich-text-editor.ts
+++ src/interfaces/rich-text-editor.ts
@@ -4,3 +4,12 @@ export interface TooltipWrapperProps {
   tooltipContent: string;
   element: JSX.Element;
 }
+
+export interface DialogWrapperProps {
+  openModal: boolean;
+  openChange: (open: boolean) => void;
+  dialogTitle: string;
+  triggerElement: JSX.Element;
+  content: JSX.Element;
+  footer: JSX.Element;
+}
