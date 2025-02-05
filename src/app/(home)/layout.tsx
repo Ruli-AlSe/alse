7e8fb6c4cffc4f59114f3687f180c6efb4c06ebf
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-02-05 03:03:16 -0600

    Rearange logic to move footer to layout page

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/components/footer';
 import { Navbar } from '@/components/navigation/navbar';
 
 export default function PortfolioLayout({ children }: { children: React.ReactNode }) {
@@ -5,6 +6,7 @@ export default function PortfolioLayout({ children }: { children: React.ReactNod
     <div className="flex flex-col items-center min-h-screen bg-gradient-to-b from-white dark:from-dark-blue from-80% to-95% to-blue-100 dark:to-light-blue">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,41 +1,29 @@
 import { EducationTimeline } from '@/components/education/timeline';
-import { Footer } from '@/components/footer';
 import { Presentation } from '@/components/user-info/presentation';
 import { JobsSection } from '@/components/professional-experience/jobs-section';
 import { SkillsSection } from '@/components/skills-showcase/skills-section';
 import { SocialMediaInfo } from '@/components/user-info/social-media';
-import { Profile } from '@/interfaces/profile';
-
-const getProfile = async (): Promise<Profile> => {
-  const api = process.env.API_URL;
-  const userEmail = process.env.USER_EMAIL;
-  const data: Profile = await fetch(`${api}/profiles/${userEmail}`).then((res) => res.json());
-
-  return data;
-};
+import { getProfile } from '../services/get-profile';
 
 export default async function Home() {
   const profile = await getProfile();
 
   return (
-    <>
-      <main className="flex flex-col gap-8 items-start md:p-16 max-w-6xl">
-        <Presentation
-          firstname={profile.name}
-          lastName={profile.last_name}
-          imageUrl={profile.image_url}
-          headliner={profile.headliner}
-          bio={profile.bio}
-        />
-        <SocialMediaInfo
-          socialMedia={profile.social_media}
-          className="w-full justify-center md:justify-end"
-        />
-        <JobsSection jobs={profile.jobs} />
-        <SkillsSection competences={profile.competences} />
-        <EducationTimeline educations={profile.education} />
-      </main>
-      <Footer socialMedia={profile.social_media} />
-    </>
+    <main className="flex flex-col gap-8 items-start md:p-16 max-w-6xl">
+      <Presentation
+        firstname={profile.name}
+        lastName={profile.last_name}
+        imageUrl={profile.image_url}
+        headliner={profile.headliner}
+        bio={profile.bio}
+      />
+      <SocialMediaInfo
+        socialMedia={profile.social_media}
+        className="w-full justify-center md:justify-end"
+      />
+      <JobsSection jobs={profile.jobs} />
+      <SkillsSection competences={profile.competences} />
+      <EducationTimeline educations={profile.education} />
+    </main>
   );
 }
