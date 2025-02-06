d827b5d39c57d48b904b86e66b897f85dfb27a0d
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-02-06 00:57:18 -0600

    Create components for handling server errors and data not found

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,27 +3,44 @@ import { Presentation } from '@/components/user-info/presentation';
 import { JobsSection } from '@/components/professional-experience/jobs-section';
 import { SkillsSection } from '@/components/skills-showcase/skills-section';
 import { SocialMediaInfo } from '@/components/user-info/social-media';
-import { getProfile } from '../services/get-profile';
+import { getProfile } from '@/services/fetch-public';
+import { NotFoundPage } from '@/components/not-found-section';
+import { ErrorPage } from '@/components/error-section';
 
 export default async function Home() {
-  const profile = await getProfile();
+  try {
+    const profile = await getProfile();
 
-  return (
-    <main className="flex flex-col gap-8 items-start md:p-16 max-w-6xl">
-      <Presentation
-        firstname={profile.name}
-        lastName={profile.last_name}
-        imageUrl={profile.image_url}
-        headliner={profile.headliner}
-        bio={profile.bio}
-      />
-      <SocialMediaInfo
-        socialMedia={profile.social_media}
-        className="w-full justify-center md:justify-end"
-      />
-      <JobsSection jobs={profile.jobs} />
-      <SkillsSection competences={profile.competences} />
-      <EducationTimeline educations={profile.education} />
-    </main>
-  );
+    if (!profile) {
+      return (
+        <NotFoundPage
+          message={`Ruli-AlSe information not found`}
+          suggestion="This is probably an error, please try again later"
+        />
+      );
+    }
+
+    return (
+      <main className="flex flex-col gap-8 items-start md:p-16 max-w-6xl">
+        <Presentation
+          firstname={profile.name}
+          lastName={profile.last_name}
+          imageUrl={profile.image_url}
+          headliner={profile.headliner}
+          bio={profile.bio}
+        />
+        <SocialMediaInfo
+          socialMedia={profile.social_media}
+          className="w-full justify-center md:justify-end"
+        />
+        <SkillsSection competences={profile.competences} />
+        <JobsSection jobs={profile.jobs} />
+        <EducationTimeline educations={profile.education} />
+      </main>
+    );
+  } catch (error: unknown) {
+    console.error(JSON.stringify(error));
+
+    return <ErrorPage message="Failed to load main page" />;
+  }
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,24 @@
 import { SocialMediaInfo } from './user-info/social-media';
-import { getProfile } from '@/services/get-profile';
+import { getProfile } from '@/services/fetch-public';
 
 export const Footer = async () => {
-  const { social_media } = await getProfile();
+  try {
+    const profile = await getProfile();
 
-  return (
-    <footer className="w-full mt-10 py-5 md:p-10 flex bg-blue-100 items-center justify-start md:justify-center border-t border-slate-400 dark:bg-light-blue">
-      <SocialMediaInfo
-        socialMedia={social_media}
-        className="w-full justify-evenly md:justify-center [&>a>span]:hidden [&>a>span]:md:block"
-      />
-    </footer>
-  );
+    return (
+      <footer className="w-full mt-10 py-5 md:p-10 flex flex-col bg-blue-100 items-center justify-start md:justify-center border-t border-slate-400 dark:bg-light-blue">
+        {profile && (
+          <SocialMediaInfo
+            socialMedia={profile.social_media}
+            className="w-full justify-evenly md:justify-center [&>a>span]:hidden [&>a>span]:md:block"
+          />
+        )}
+        <p className="my-9">{new Date().getFullYear()} © All rights reserved</p>
+      </footer>
+    );
+  } catch (error: unknown) {
+    console.error(JSON.stringify(error));
+
+    return null;
+  }
 };
